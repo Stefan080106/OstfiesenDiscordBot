491e867f323e8360eb743745c3e958dfc18e44b7
stefan080106 <stefan.arnold.programmierung@gmail.com>
2021-03-29 17:30:39 +0200

    ✨feature: update ping cmd

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,13 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-               message.channel.send('pong');
-}
+	message.channel.send(
+		`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(
+			bot.ws.ping
+		)}ms`
+	);
+};
 
 module.exports.help = {
-  name:"ping"
-}
+	name: "ping",
+};
